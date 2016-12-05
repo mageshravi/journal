3696702b9110677643ab1d0f8db2cc33beb3d52f
Magesh Ravi <magesh@webinative.com>
2016-12-05 23:20:12 +0000

    Latest posts first
    - Orders the posts by published date
    - Renames Personal Log as Journal in the template

--- posts/services.py
+++ posts/services.py
@@ -5,4 +5,6 @@ class PostService(object):
 
     def get_all_published(self):
 
-        return Post.objects.all().filter(is_published=True)
+        return Post.objects.all().filter(
+            is_published=True
+        ).order_by('-published_on')
